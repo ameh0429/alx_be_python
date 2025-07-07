bddfdff6ce8985200ad145f580b9dd5c8911ea88
amehmathias <amehmathiasejeh40@gmail.com>
2025-07-07 15:29:22 +0100

    Solved simple basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+def main():
+    number1 = 10
+    number2 = 5
+
+    addition = number1 + number2
+    print(f"Addition of {number1} and {number2} is {addition}")
+
+    subtraction = number1 - number2
+    print(f"Subtraction of {number1} and {number2} is {subtraction}")
+
+    multiplication = number1 * number2
+    print(f"Multiplication of {number1} and {number2} is {multiplication}")
+
+if __name__ == "__main__":
+    main()
