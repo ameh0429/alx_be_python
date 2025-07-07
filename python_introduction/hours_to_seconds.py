b70ad42827d4cf332b912a91db84e1e742f430ce
amehmathias <amehmathiasejeh40@gmail.com>
2025-07-07 18:25:52 +0100

    Calculated number of seconds in 2 hour

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+hours = 2
+
+# convert hours to seconds
+# since there are 60 minutes in an hour and 60 seconds in a minute, thus 60 x 60 = 3600
+seconds = hours * 3600
+
+# print the result
+print(f"{hours} hour(s) is {seconds} seconds")
